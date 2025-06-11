72b5c28e3e7c1aa97a64bad22e8e261ad69e07fd
Chihen-Tai <microdaery@gapp.nthu.edu.tw>
2025-06-12 02:50:44 +0800

    clear code

--- chem/data/general_chem.js
+++ chem/data/general_chem.js
@@ -4,17 +4,17 @@ const pageData = {
     {
       name: "上學期・廖文峯",
       files: [
-        { name: "期中考 I", file: "midterm_template.html?subject=general_chem&exam=midterm1" },
-        { name: "期中考 II", file: "midterm_template.html?subject=general_chem&exam=midterm2" },
-        { name: "期末考", file: "midterm_template.html?subject=general_chem&exam=midterm3" }
+        { name: "期中考 I", file: "../midterm_template.html?subject=general_chem&exam=midterm1" },
+        { name: "期中考 II", file: "../midterm_template.html?subject=general_chem&exam=midterm2" },
+        { name: "期末考", file: "../midterm_template.html?subject=general_chem&exam=midterm3" }
       ]
     },
     {
       name: "下學期",
       files: [
-        { name: "期中考 I", file: "midterm_template.html?subject=general_chem&exam=midterm1b" },
-        { name: "期中考 II", file: "midterm_template.html?subject=general_chem&exam=midterm2b" },
-        { name: "期末考", file: "midterm_template.html?subject=general_chem&exam=finalb" }
+        { name: "期中考 I", file: "../midterm_template.html?subject=general_chem&exam=midterm1b" },
+        { name: "期中考 II", file: "../midterm_template.html?subject=general_chem&exam=midterm2b" },
+        { name: "期末考", file: "../midterm_template.html?subject=general_chem&exam=finalb" }
       ]
     }
   ]
