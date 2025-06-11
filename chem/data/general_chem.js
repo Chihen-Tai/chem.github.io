db6bd6d55bf56afcc99ff266c4f9a511e4e47c7a
Chihen-Tai <microdaery@gapp.nthu.edu.tw>
2025-06-12 02:42:44 +0800

    clear code

---
+++ chem/data/general_chem.js
@@ -0,0 +1,21 @@
+const pageData = {
+  title: "📘 普通化學 考古題分類",
+  semesters: [
+    {
+      name: "上學期・廖文峯",
+      files: [
+        { name: "期中考 I", file: "../midterm_template.html?subject=general_chem&exam=midterm1" },
+        { name: "期中考 II", file: "midterm_template.html?subject=general_chem&exam=midterm2" },
+        { name: "期末考", file: "midterm_template.html?subject=general_chem&exam=midterm3" }
+      ]
+    },
+    {
+      name: "下學期",
+      files: [
+        { name: "期中考 I", file: "exam_viewer.html?subject=general_chem&exam=midterm1b" },
+        { name: "期中考 II", file: "exam_viewer.html?subject=general_chem&exam=midterm2b" },
+        { name: "期末考", file: "exam_viewer.html?subject=general_chem&exam=finalb" }
+      ]
+    }
+  ]
+};
---
+++ chem/data/general_chem_midterm1.js
@@ -0,0 +1,13 @@
+const pageData = {
+  title: "🧪 普通化學 - 上學期 期中考 I",
+  papers: [
+    {
+      name: "2023 年期中考題",
+      path: "files/general_Chemistry/4_14_print.pdf"
+    },
+    {
+      name: "2022 年期中考題",
+      path: "files/general_Chemistry/midterm1_2022.pdf"
+    }
+  ]
+};
