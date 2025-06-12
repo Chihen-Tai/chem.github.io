4cf91de7e01ad04ea4439dcb655d76e3d392e048
Chihen-Tai <microdaery@gapp.nthu.edu.tw>
2025-06-12 21:07:41 +0800

    123

--- chem/data/chem_math/chem_math.js
+++ chem/data/chem_math/chem_math.js
@@ -2,19 +2,19 @@ const pageData = {
   title: "化學數學 考古題",
   semesters: [
     {
-      name: "上學期",
+      name: "作業",
+      files: [
+        { name: "作業 1", file: "../assignment_template.html?subject=chem_math&assignment=assignment1" },
+        { name: "作業 2", file: "../assignment_template.html?subject=chem_math&assignment=assignment2" },
+        { name: "作業 3", file: "../assignment_template.html?subject=chem_math&assignment=assignment3" }
+      ]
+    },
+    {
+      name: "期中考",
       files: [
         { name: "期中考 I", file: "../midterm_template.html?subject=organic_chem&exam=midterm1" },
         { name: "期中考 II", file: "../midterm_template.html?subject=organic_chem&exam=midterm2" },
         { name: "期末考", file: "../midterm_template.html?subject=organic_chem&exam=midterm3" }
-      ]
-    },
-    {
-      name: "下學期",
-      files: [
-        { name: "期中考 I", file: "../midterm_template.html?subject=organic_chem&exam=midterm1b" },
-        { name: "期中考 II", file: "../midterm_template.html?subject=organic_chem&exam=midterm2b" },
-        { name: "期末考", file: "../midterm_template.html?subject=organic_chem&exam=finalb" }
       ]
     }
   ]
